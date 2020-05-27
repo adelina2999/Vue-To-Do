4cd9d07dd7561cade81ff14667316c011c82cd89
adelina2999 <tataru.adelina99@gmail.com>
2020-05-27 14:04:08 +0300

    User events

--- src/components/Content/Content.vue
+++ src/components/Content/Content.vue
@@ -1,12 +1,35 @@
 <template>
-  <div>
-    Content    
+  <div id="shopping-list">
+    <h1>{{ header.toLocaleUpperCase() }}</h1>
+    <div class="add-item-form">
+            <!-- @keyup.enter adauga elemente din input prin enter -->
+      <input 
+        v-model="newItem" type="text" 
+        placeholder="Add an item" 
+        @keyup.enter="items.push(newItem)">
+      <button class="btn btn-primary" @click="items.push(newItem)">Save Item</button>
+    </div>
+    <ul>
+      <li v-for="item in items" 
+        :key="item">{{ item }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Content'
+  name: 'Content',
+  data() {
+    return {
+      header: 'Shopping List App',
+      newItem: '',
+      items: [
+        '10 party hats',
+        '2 board games',
+        '20 cups'
+      ]
+    }
+  }
 }
 </script>
 
