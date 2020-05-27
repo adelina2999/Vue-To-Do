98775382e01f80dc303407b7dcc3d476cb3953ed
adelina2999 <tataru.adelina99@gmail.com>
2020-05-27 15:23:06 +0300

    Attribute Bindings

--- src/components/Content/Content.vue
+++ src/components/Content/Content.vue
@@ -1,18 +1,22 @@
 <template>
   <div id="shopping-list">
-    <h1>{{ header.toLocaleUpperCase() }}</h1>
-    <div class="add-item-form">
-            <!-- @keyup.enter adauga elemente din input prin enter -->
+    <div>
+      <h1>{{ header.toLocaleUpperCase() }}</h1>
+      <button v-if="state === 'default'" class="btn btn-primary" @click="changeState('edit')">Add item</button>
+      <button v-else class="btn btn-cancel" @click="changeState('default')">Cancel</button>
+    </div>
+    <div v-if="state === 'edit'" class="add-item-form">
       <input 
         v-model="newItem" type="text" 
         placeholder="Add an item" 
-        @keyup.enter="items.push(newItem)">
+        @keyup.enter="saveItem">
       <button class="btn btn-primary" @click="items.push(newItem)">Save Item</button>
     </div>
     <ul>
       <li v-for="item in items" 
         :key="item">{{ item }}</li>
     </ul>
+    <p v-if="items.length === 0">Nice job! You've bough all your items</p>
   </div>
 </template>
 
@@ -21,13 +25,25 @@ export default {
   name: 'Content',
   data() {
     return {
+      state: 'default',
       header: 'Shopping List App',
       newItem: '',
       items: [
-        '10 party hats',
-        '2 board games',
-        '20 cups'
+        // '10 party hats',
+        // '2 board games',
+        // '20 cups'
       ]
+    }
+  },
+  methods: {
+    saveItem() {
+      this.items.push(this.newItem)
+      this.newItem = ''
+    },
+    changeState(newState) {
+      this.state = newState
+
+      this.newItem = ''
     }
   }
 }
